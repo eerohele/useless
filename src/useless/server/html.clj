4be4007da55736c25e709c5abd4a6131a0d38977
Eero Helenius <eero.helenius@pm.me>
2019-09-11 10:27:37 +0300

    Add support for Jekyll front matter

--- src/useless/server/html.clj
+++ src/useless/server/html.clj
@@ -1,18 +1,22 @@
 (ns useless.server.html
-  (:require [clojure.java.io :as io]
-            [selmer.parser :as parser]
+  (:require [selmer.parser :as parser]
             [selmer.util :as util])
   (:import (com.vladsch.flexmark.parser Parser)
            (com.vladsch.flexmark.html HtmlRenderer)
            (com.vladsch.flexmark.util.data MutableDataSet)
            (com.vladsch.flexmark.ext.tables TablesExtension)
-           (com.vladsch.flexmark.ext.gfm.tasklist TaskListExtension)))
+           (com.vladsch.flexmark.ext.gfm.tasklist TaskListExtension)
+           (com.vladsch.flexmark.ext.jekyll.front.matter JekyllFrontMatterExtension)))
+
+
+(def ^:private flexmark-extensions
+  [(TablesExtension/create) (TaskListExtension/create) (JekyllFrontMatterExtension/create)])
 
 
 (defn markdown->html-string
   [input-string]
   (let [options (MutableDataSet.)
-        parser (.build (.extensions (Parser/builder options) [(TablesExtension/create) (TaskListExtension/create)]))
+        parser (.build (.extensions (Parser/builder options) flexmark-extensions))
         renderer (.build (HtmlRenderer/builder options))
         document (.parse parser input-string)]
     (.render renderer document)))
