90f028e12cc924bb0c8606720323bd3dac579edc
Eero Helenius <eero.helenius@pm.me>
2019-11-23 18:14:45 +0200

    Remove unused requires

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,5 @@
 (ns user
-  (:require [clojure.java.io :as io]
-            [integrant.core :as ig]
-            [integrant.repl :as repl :refer [clear go halt prep init reset reset-all]]
+  (:require [integrant.repl :as repl :refer [clear go halt prep init reset reset-all]]
             [useless.server.app :as app]))
 
 
